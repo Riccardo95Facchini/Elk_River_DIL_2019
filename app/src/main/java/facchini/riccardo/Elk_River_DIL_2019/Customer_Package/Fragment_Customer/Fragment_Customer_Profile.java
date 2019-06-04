42d579ee4fe5ee40cc0d7c623789a2751f01460b
Riccardo Facchini <riccardo95facchini@gmail.com>
2019-06-04 14:52:48 +0200

    Added Picasso library and image display on customer profile

--- app/src/main/java/facchini/riccardo/Elk_River_DIL_2019/Customer_Package/Fragment_Customer/Fragment_Customer_Profile.java
+++ app/src/main/java/facchini/riccardo/Elk_River_DIL_2019/Customer_Package/Fragment_Customer/Fragment_Customer_Profile.java
@@ -1,15 +1,18 @@
 package facchini.riccardo.Elk_River_DIL_2019.Customer_Package.Fragment_Customer;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import facchini.riccardo.Elk_River_DIL_2019.R;
 import facchini.riccardo.Elk_River_DIL_2019.SharedViewModel;
@@ -20,6 +23,7 @@ public class Fragment_Customer_Profile extends Fragment
     private SharedViewModel viewModel;
     
     private TextView profileInfoText;
+    private ImageView imageView;
     
     @Nullable
     @Override
@@ -34,6 +38,9 @@ public class Fragment_Customer_Profile extends Fragment
     {
         super.onActivityCreated(savedInstanceState);
         viewModel = ViewModelProviders.of(getActivity()).get(SharedViewModel.class);
+        
+        Picasso.get().load(Uri.parse(viewModel.getCurrentCustomer().getProfilePicUrl())).fit().centerCrop().into(imageView);
+        
         profileInfoText.setText(viewModel.getCurrentCustomer().displayProfile());
     }
     
@@ -41,5 +48,6 @@ public class Fragment_Customer_Profile extends Fragment
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
     {
         profileInfoText = view.findViewById(R.id.profileInfoText);
+        imageView = view.findViewById(R.id.imageView);
     }
 }
